7e212bf2d38666e50390193b6ce7b6f019302f75
Greg Hardin <greg.hardin@live.com>
2016-05-12 11:47:54 -0700

    Delete main.js

--- TypeScriptKnockout/scripts/built/main.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports", "./helloworld", "./calc"], function (require, exports, helloworld_1, calc_1) {
-    "use strict";
-    var calc = new calc_1.Calc(8, 4);
-    console.log(calc.add());
-    console.log(calc.subtract());
-    var hello = new helloworld_1.Hello("Dev", "Team");
-    console.log(hello.sayHello());
-});
-//# sourceMappingURL=main.js.map
